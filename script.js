858096e817a2234eedb79918e44719feb8a837ed
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-20 19:51:01 -0700

    add button roll dice and button game info hide toggle

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 "use strict"
 
-
-let randomNumber1 = Math.floor(Math.random() * 6 + 1);
-let randomNumber2 = Math.floor(Math.random() * 6 + 1);
-
-document.getElementById("die1").setAttribute("src", "assets/images/dice" + randomNumber1 + ".png");
-document.getElementById("die2").setAttribute("src", "assets/images/dice" + randomNumber2 + ".png");
-
 function displayWinner() {
+    let randomNumber1 = Math.floor(Math.random() * 6 + 1);
+    let randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+    document.getElementById("die1").setAttribute("src", "assets/images/dice" + randomNumber1 + ".png");
+    document.getElementById("die2").setAttribute("src", "assets/images/dice" + randomNumber2 + ".png");
+
     if (randomNumber1 > randomNumber2) {
         document.getElementById("hero").innerText = "🚩 Player 1 Wins!"
     } else if (randomNumber1 < randomNumber2) {
@@ -17,4 +16,14 @@ function displayWinner() {
     }
 }
 
-displayWinner();
+document.getElementById("tossButton").addEventListener("click", displayWinner);
+
+document.querySelector(".gameInfo").addEventListener("click", function () {
+    let info = document.querySelector(".gameInfoContent");
+    if (info.style.display === "none") {
+        info.style.display = "block";
+    } else {
+        info.style.display = "none";
+    }
+})
+
